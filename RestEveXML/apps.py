08b1743122092e07349c2f0cde5a29206c415822
Sven <matzke.sven@googlemail.com>
2016-02-22 21:37:37 +0100

    just the creation of the used django apps and project

---
+++ RestEveXML/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RestevexmlConfig(AppConfig):
+    name = 'RestEveXML'
---
+++ install_packages.bat
@@ -0,0 +1,6 @@
+python -m pip install --upgrade pip
+python -m pip install --upgrade setuptools
+python -m pip install --upgrade django
+python -m pip install --upgrade djangorestframework
+
+
